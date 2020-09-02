8a605837787e4dab4c58e4873c0d41a324e9e4ae
Mohammad Zubair <zubairdumca09@gmail.com>
2020-09-02 08:53:24 +0530

    changes

---
+++ PlainJs/chaining.js
@@ -0,0 +1,13 @@
+let ladder = {
+  step: 0, up() {
+    this.step++;
+    return this;
+  }, down() {
+    this.step--;
+    return this;
+  }, showStep() {
+    alert(this.step);
+    return this;
+  }
+}
+ladder.up().up().down().up().down().showStep();
